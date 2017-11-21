ffe3cc0706c5f0848237facb154926961b205d62
Islenauto <acselroad@gmail.com>
2017-11-21 19:56:54 +0900

    Update: func=show topic of hdp model

--- script/main.py
+++ script/main.py
@@ -1,18 +1,20 @@
 # -*- encoding: UTF-8 -*-
-import os,sys
+import os,sys,re
 root_path = os.path.dirname(os.getcwd())
 sys.path.append(root_path+"/mylib/")
-import lda 
+import lda,datacontroler as dtct
 
 
 def main():
     num_topics = sys.argv[1]
     name_news = sys.argv[2]
+    category = sys.argv[3]
 
-    model_lda = lda.Lda(num_topics,name_news).lda
+    model_lda = lda.Lda(num_topics,name_news,category)
 
-    for topic in model_lda.show_topics(-1):
-        print (topic)
+    result_topics = [topic for topic in model_lda.hdp.show_topics(-1)]
+    dtct.DataControler().export_data(result_topics,name_news,category,mode="result")
+
 
 if __name__=='__main__':
     main()
