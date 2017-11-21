6cf50cdd37f9c0c8861efc4f321e9bccecd419e6
Islenauto <acselroad@gmail.com>
2017-11-21 19:58:03 +0900

    Update: hdp model

--- mylib/lda.py
+++ mylib/lda.py
@@ -2,14 +2,17 @@
 from gensim import corpora,models,similarities
 import nltk
 import treetaggerwrapper as ttw
-import re,os
-import csv
+import re,sys,os
+import datacontroler as dtct
 
 root_path = os.path.dirname(os.getcwd())
 
 class Lda:
-    def __init__(self,num_topics,name_news):
-        content_dic = self.import_data(name_news)
+    def __init__(self,num_topics,name_news,category):
+        self.name_news = name_news
+        self.category = category
+
+        content_dic = dtct.DataControler().import_data(self.name_news,self.category)
         texts = [dic['sentence'] for dic in content_dic]
 
                                                         
@@ -21,18 +24,6 @@ class Lda:
         tfidf = models.TfidfModel(corpus)
         corpus_tfidf = tfidf[corpus]
         
-        #corpora.MmCorpus.serialize(root_path+"/data/bcc.mm",corpus)
-        #corpus = corpora.MmCorpus('./data/bcc.mm')
-
         # gensimのldaモデル作成
         self.lda = models.ldamodel.LdaModel(corpus=corpus_tfidf, num_topics=num_topics,id2word=dictionary)
-
-    # csvから辞書データを読み込む
-    def import_data(self,name_news):
-        with open(root_path+"/data/contents_" + name_news + ".csv","r") as f:
-            reader = csv.DictReader(f)
-            content_dic = [row for row in reader]
-            for dic in content_dic:
-                target = dic['sentence']
-                dic['sentence'] = re.sub("'|\[|\]|\s]","",target).split(",")
-            return content_dic
+        self.hdp = models.hdpmodel.HdpModel(corpus=corpus_tfidf,id2word=dictionary)
