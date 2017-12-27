16481d900762d67c5560a9865a314256c24df37f
Islenauto <acselroad@gmail.com>
2017-12-27 09:11:43 +0900

    Update: first-relevance [calc_score_label]

--- mylib/grantlabel.py
+++ mylib/grantlabel.py
@@ -1,42 +1,55 @@
 # -*- encoding: UTF-8 -*-
 import os,sys
 import nltk,pandas,numpy
-import mylib.topicmodel
+from tqdm import tqdm
+from gensim import corpora,models
+from mylib.topicmodel import TopicModel
 from mylib.datacontroler import DataControler
+from mylib.ngram import Ngram
 
 class GrantLabel:
-    def __init__(self,topicmodel,method=0):
+    def __init__(self,topicmodel,method):
 
+        data_cont = DataControler()
         self.topic_model = topicmodel
-        self.C = self.topic_model.data_parsed # 文脈情報に用いるテキストデータ
+        self.method = method
         self.W_Theta = self.topic_model.W_Theta # トピック毎の単語生起確率リスト
-        self.labels = DataControler().flatten_list([list(nltk.bigrams(text)) for text in self.C]) # ラベル候補(bi-gram)
-        self.labels_scored = []
+        self.C = self.topic_model.data_parsed # 文脈情報に用いるコーパスC
+        self.ngram = Ngram(self.C,n=2) # ngramの言語モデルを作成                        
 
+        self.labels = list(set(data_cont.flatten_list(self.ngram.texts_ngram)))
+        self.labels_scored = [] # スコアリングしたラベルの辞書をトピック毎に格納するリスト
         self.calc_score_labels(method)
 
 
-    def calc_score_label(self,label,W_theta,method):
+    def calc_score_label(self,label,W_theta):
         
-        score = 0
-        # zero-order relevance
-        if method == 0:
+        score = 0      
+        # zero-order relevance (normalize:uniform dist)
+        if self.method == 0:                
             for word in label:
                 score += numpy.log(W_theta[word])
-
+        
         # first-order relevance
-        elif method == 1:
-            for word in label:
-                score += 1
-
+        elif self.method == 1:
+            label = '-'.join(label)
+            for word, w_theta in tqdm(W_theta.items()):
+                w_C = self.dic_mle_1gram[word] # コーパスCでの単語wの生起確率(最尤推定量)
+                l_C = self.dic_mle_ngram[label] # コーパスCでのラベルlの生起確率(最尤推定量)
+                cooccur_wl = self.ngram.count_cooccur(word,label,search_window=10,complex_term=True) # w,lの共起頻度
+                wl_C = cooccur_wl / (len(self.W_Theta[0].keys()) - self.ngram.n + 1)
+                score += w_theta * numpy.log(wl_C + 1 / (w_C * l_C))
+        
         return score
     
     
     def calc_score_labels(self,method):
-        
-        for W_theta in self.W_Theta:
-            labels_scored_at_theta = {' '.join(label):self.calc_score_label(label,W_theta,method) for label in self.labels}
-            self.labels_scored.append(labels_scored_at_theta)
+               
+        self.dic_mle_1gram = Ngram(self.C,n=1).mle()
+        self.dic_mle_ngram = self.ngram.mle()
+        for W_theta in tqdm(self.W_Theta):
+            labels_scored_theta = {' '.join(label):self.calc_score_label(label,W_theta) for label in self.labels}
+            self.labels_scored.append(labels_scored_theta)
 
 
     def show_labels(self,id_topic,num_labels=10):
