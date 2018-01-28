a4cb6c080a6f2aed39b1762db60834f906d79d56
Islenauto <acselroad@gmail.com>
2018-01-29 00:15:57 +0900

    [Update]"init_labels"にt値による選定処理を追加
    各ラベルのt値の上位1,000件をラベル候補として抽出する処理を追加

--- mylib/grantlabel.py
+++ mylib/grantlabel.py
@@ -4,6 +4,7 @@ import nltk,pandas,numpy
 import treetaggerwrapper as ttw
 from itertools import chain
 from tqdm import tqdm
+from operator import itemgetter
 from gensim import corpora,models
 from mylib.topicmodel import TopicModel
 from mylib.ngram import Ngram
@@ -16,26 +17,36 @@ class GrantLabel:
         self.method = method
         self.W_Theta = self.topic_model.W_Theta_indoc # トピック毎の単語生起確率リスト
         self.C = self.topic_model.sentences_parsed # 文脈情報に用いるコーパスC
-        self.ngram = Ngram(self.C,n=2) # ngramの言語モデルを作成                        
-
+        self.ngram = Ngram(self.C,n=2) # ngramの言語モデルを作成        
+        self.dic_cooccur_wl = pandas.read_csv("../data/cooccur_wl_{0}gram.csv".format(self.ngram.n),index_col=0,encoding="cp932")
+        
         #self.labels = self.init_labels(tag_stopwd=['CC','DT','IN','MD','RB'])
-        self.labels = set(list(chain.from_iterable(self.ngram.texts_ngram)))
+        self.labels = self.init_labels(tag_stopwd=[])
+        pandas.DataFrame(["-".join(label) for label in self.labels]).to_csv("../data/labels_candidate.csv")
+        #self.labels = set(list(chain.from_iterable(self.ngram.texts_ngram)))
         self.labels_scored = {} # スコアリングしたラベルの辞書をトピック毎に格納する辞書(hashkey=トピック番号)
         self.calc_score_labels(method)
 
 
-    # ラベル候補を作成(tag_stopwdで指定した品詞を含むラベルは排除)
+    # ラベル候補を作成(品詞とtスコアによる選定)
     def init_labels(self,tag_stopwd):
         
         tagdir = os.getenv('TREETAGGER_ROOT')
         tagger = ttw.TreeTagger(TAGLANG='en',TAGDIR=tagdir)
 
         labels = set(list(chain.from_iterable(self.ngram.texts_ngram)))
+        dic_cooccur = self.ngram.make_dic_cooccur(self.topic_model.W,self.topic_model.W,self.C)
+        
         new_labels = []
         for label in labels:
-            pos_results = [result.split('\t')[1] for result in tagger.TagText(' '.join(label))]
-            if list(set(pos_results) & set(tag_stopwd)) == []: new_labels.append(label) 
-        return new_labels
+            #pos_results = [result.split('\t')[1] for result in tagger.TagText(' '.join(label))]
+            #if list(set(pos_results) & set(tag_stopwd)) == []: 
+            t = self.ngram.t_score(label[0],label[1],dic_cooccur)
+            new_labels.append((label,t))
+        new_labels.sort(key=itemgetter(1),reverse=True)
+        new_labels = [label for label,t in new_labels]
+
+        return new_labels[:1000]
 
 
     def calc_score_label(self,label,id_topic,W_theta):
@@ -48,11 +59,11 @@ class GrantLabel:
         
         # first-order relevance
         elif self.method == 1:
-            label = '-'.join(label)
+            label = '-'.join(label)                                                              
             for word, w_theta in tqdm(W_theta.items()):
                 w_C = self.dic_mle_1gram[word] # コーパスCでの単語wの生起確率(最尤推定量)
                 l_C = self.dic_mle_ngram[label] # コーパスCでのラベルlの生起確率(最尤推定量)
-                cooccur_wl = self.dic_cooccur[word][label] # w,lの共起頻度
+                cooccur_wl = self.dic_cooccur_wl[word][label] # w,lの共起頻度
                 wl_C = cooccur_wl + 1 / (len(self.W_Theta[id_topic].keys()) - self.ngram.n + 1)
                 score += w_theta * numpy.log2(wl_C / (w_C * l_C)) # w_theta * PMI(w,l|C)
                 score -= w_theta * numpy.log2(w_theta / w_C) # KLダイバージェンス(トピック-コーパスC)
@@ -65,8 +76,8 @@ class GrantLabel:
         if method == 1:
             self.dic_mle_1gram = Ngram(self.C,n=1).mle()
             self.dic_mle_ngram = self.ngram.mle()
-            labels = ["-".join(label) for label in self.labels ]
-            self.dic_cooccur = self.ngram.make_dic_cooccur(self.topic_model.W,labels,self.C,search_window=30,complex_term=True)
+            labels = ["-".join(label) for label in self.labels]
+            #self.dic_cooccur = self.ngram.make_dic_cooccur(self.topic_model.W,labels,self.C,search_window=30,complex_term=True)
 
         for id_topic,W_theta in tqdm(self.W_Theta.items()):
             labels_scored_theta = {' '.join(label):self.calc_score_label(label,id_topic,W_theta) for label in tqdm(self.labels)}
