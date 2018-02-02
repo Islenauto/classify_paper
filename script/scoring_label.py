b4c47b142860d67778d14210355f1f193e0febb1
Islenauto <acselroad@gmail.com>
2018-02-02 18:10:48 +0900

    refactoring

--- script/scoring_label.py
+++ script/scoring_label.py
@@ -25,8 +25,8 @@ def main():
     
     id_topics = topic_model.id_topics_indoc
     for i in id_topics:
-        labels_dic = grant_label.show_labels(id_topic=i,num_labels=100)
-        pd.DataFrame(labels_dic).to_csv("{0}/result/labels/2gram/{1}/topic_{2}.csv".format(root_path,category,i))
+        labels_dic = grant_label.show_labels(id_topic=i,num_labels=1000)
+        pd.DataFrame(labels_dic).to_csv("{0}/result/labels/2gram/first_order_relevance/{1}/topic_{2}.csv".format(root_path,category,i))
 
 
 if __name__=='__main__':
