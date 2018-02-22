40b051d0580e8a0bd801c4d3035301c496ac570c
Islenauto <acselroad@gmail.com>
2018-02-22 22:19:35 +0900

    [Add]insert_readed_articles
    指定した閲覧済み記事集合を１つの記事とし，推薦候補の記事群に追加してモデルを生成できるようにした

--- script/update_model.py
+++ script/update_model.py
@@ -17,9 +17,19 @@ def input_opr():
     return (argv[1],argv[2])
 
 
+def insert_readed_articels(tb_recomm_articles,tb_readed_articles):
+
+    sent_flatten = "\t".join(list(tb_readed_articles["sentence_parsed"]))
+    sent_pos_flatten = "\t".join(list(tb_readed_articles["sentence_parsed_with_pos"]))
+    tb_temp = pd.DataFrame({"sentence_parsed":sent_flatten,"sentence_parsed_with_pos":sent_pos_flatten},index=[0])
+    #tb_inserted = pd.concat([tb_recomm_articles,tb_temp])
+    tb_inserted = tb_temp
+    return tb_inserted
+
+
 if __name__=="__main__":
     name,category = input_opr()
 
     db = PaperRails()
-    tb_articles = db.format_tb_articles(db.get_tb_articles(name,category,date(2016,12,1),date(2016,12,13)))
+    tb_articles = db.format_tb_articles(db.get_tb_articles(name,category,date(2017,12,1),date(2017,12,31)))
     TopicModel(tb_articles,update=True)
